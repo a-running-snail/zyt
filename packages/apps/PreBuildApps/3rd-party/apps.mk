a94e58ea17f2bf9fa159e9ab837fe00ac1b5d187
mafei <18218724438@163.com>
2019-05-30 22:24:35 +0800

    package\system

---
+++ packages/apps/PreBuildApps/3rd-party/apps.mk
@@ -0,0 +1,10 @@
+#prize-public add 3rdparty apps to system-lanwm-20180830
+LOCAL_PATH :=$(TOPDIR)packages/apps/PreBuildApps/3rd-party
+
+#ifeq ($(strip $(PRIZE_XXXX_XXXX)), yes)
+#    PRODUCT_PACKAGES += XXXX_XXXX.apk
+#    PRODUCT_COPY_FILES += packages/apps/PreBuildApps/public/xxxx/xxxx:system/xxxx/xxxx
+#endif
+
+
+
